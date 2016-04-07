0ce95f5b15c77ded72d31141dbba94e7ffa20637
Colin Anderson <andercol.4@gmail.com>
2016-04-07 12:40:57 -0600

    the quotes index paginates at 10

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,6 @@
 class QuotesController < InheritedResources::Base
+  # max_paginates_per 10
   def index
-    @quotes = Quote.page params[:page]
+    @quotes = Quote.page(params[:page]).per(10)
   end
 end
