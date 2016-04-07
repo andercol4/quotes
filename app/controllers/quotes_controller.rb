5517e954296e0210b47fa690de2b8e416b65606c
Colin Anderson <andercol.4@gmail.com>
2016-04-07 13:16:53 -0600

    ordered the quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,6 @@
 class QuotesController < InheritedResources::Base
   # max_paginates_per 10
   def index
-    @quotes = Quote.page(params[:page]).per(10)
+    @quotes = Quote.ordered_quote.page(params[:page]).per(10)
   end
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,10 @@
 class Quote < ActiveRecord::Base
   belongs_to :user
   belongs_to :source
+
+  def self.ordered_quote
+    select("quotes.*")
+    .joins("INNER JOIN sources s ON s.id = quotes.source_id")
+    .order("s.name")
+  end
 end
