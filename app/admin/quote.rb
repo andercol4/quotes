b975e552172568618a9df9ab3fe0a7c133a9042b
Colin Anderson <andercol.4@gmail.com>
2016-04-07 10:30:00 -0600

    gave active admin an update so it has actual model attributes

--- app/admin/quote.rb
+++ app/admin/quote.rb
@@ -1,26 +1,17 @@
 ActiveAdmin.register Quote do
-  permit_params :body
+permit_params :body, :user_id, :source_id
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
 
-  index do |i|
-    i.column :body
 
-    i.actions
-  end
-
-  form do |f|
-    f.inputs 'Quote' do
-      # f.source
-      f.input :body
-    end
-    f.actions
-  end
-
-  show do |s|
-    attributes_table do
-      # show user and source here
-      row 'quote body' do
-        s.body
-      end
-    end
-  end
 end
--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,27 +1,17 @@
 ActiveAdmin.register User do
-  permit_params :first_name, :last_name, :email, :password
 
-  filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
 
-  index do |i|
-    i.column :first_name
-    i.column :last_name
-    i.column :email
 
-    i.actions
-  end
-
-  form do |f|
-    f.inputs "User Details" do
-      f.input :first_name
-      f.input :last_name
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
-    end
-    f.actions
-  end
 end
