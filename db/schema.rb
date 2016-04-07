c9982f9eb480451d811ff9aec72ef56bf0b0ee91
Colin Anderson <andercol.4@gmail.com>
2016-04-07 07:58:47 -0600

    added associations to migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404180231) do
+ActiveRecord::Schema.define(version: 20160407135616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20160404180231) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "source_id"
   end
 
   create_table "sources", force: :cascade do |t|
